41f3bd6b9d8ec6098043d19a4082cf3cd99b95db
IWUCHUKWU IKECHUKWU <iwuchukwuonline@gmail.com>
2023-06-19 13:33:30 -0700

    ORM using python in db

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.sql
@@ -0,0 +1,2 @@
+-- This will Insert states
+INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
---
+++ 0x0F-python-object_relational_mapping/README.md
@@ -0,0 +1,1 @@
+this is a project task on 0x0F. Python - Object-relational mapping
